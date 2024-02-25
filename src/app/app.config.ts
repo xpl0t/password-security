3e4e3f9cd681c61eb2178656ab441cfd5d0322cc
xpl0t <sheepr4ider@gmail.com>
2024-02-25 19:08:38 +0100

    feat: added service worker

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideAnimations()
-  ]
+    provideAnimations(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 };
